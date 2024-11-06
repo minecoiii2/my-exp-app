f2ef5104f4728afa18f80f7884cbb2aa9956abd1
minecoiii2 <raph.didier15@gmail.com>
2024-11-06 18:51:51 +0100

    html fix

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ var server = http.createServer(function(req, res) {
                 return;
             }
            
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end(data);
         });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File Not Found')
     }
 });
 
