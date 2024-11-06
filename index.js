1ec42e5d75c7407423d22d0e89804dafdc2ec478
minecoiii2 <raph.didier15@gmail.com>
2024-11-06 17:39:49 +0100

    updt for namecheap

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = 80;
-
-app.get('/cool', (req, res) => {
-    res.send('Welcome to my website! it is currently very sigma');
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on thing thing i guess`);
-});
+server.listen();
