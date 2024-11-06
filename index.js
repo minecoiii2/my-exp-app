72b34a9faf7abee6c9c93d315dd03969b2dd49fa
minecoiii2 <raph.didier15@gmail.com>
2024-11-06 17:41:26 +0100

    updt for namecheap2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+    if (req.url.endsWith("cool")) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        var response = "yo whats good guys!!!";
+        res.end(response);
+    }
 });
 server.listen();
