bb4cd2aed3f164dd13dcce32dc7202be4e28ef3f
minecoiii2 <raph.didier15@gmail.com>
2024-11-06 18:14:47 +0100

    h

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var http = require('http');
+var url = require('url');
+
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It yay!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+    var parsedUrl = url.parse(req.url, true);  // Parse the URL of the request
+    
+    // Check if the requested path is '/cool'
+    if (parsedUrl.pathname === '/cool') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        var message = 'It yay!\n',
+            version = 'NodeJS ' + process.versions.node + '\n',
+            response = [message, version].join('\n');
+        res.end(response);
+    } else {
+        // If the path is not '/cool', respond with a 404 error
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found');
+    }
 });
-server.listen();
+
+// Listen on port 80 for production
+server.listen(80, function() {
+    console.log('Server is listening on port 80');
+});
