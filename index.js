619c9259038f7a36f23cdb6bcd120581d99d893d
minecoiii2 <raph.didier15@gmail.com>
2024-11-06 18:00:15 +0100

    should be good now

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    if (req.url.endsWith("cool")) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        var response = "yo whats good guys!!!";
-        res.end(response);
-    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var response = "yo whats good guys!!!";
+    res.end(response);
 });
 server.listen();
